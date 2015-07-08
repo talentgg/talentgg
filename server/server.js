a1c09d36ffbc6a2708df17227574dbcb0d460482
bryan <bry_angelo@yahoo.com>
2015-07-08 14:37:56 -0700

    refactored for user specific routes, added controller file

--- server/server.js
+++ server/server.js
@@ -6,6 +6,9 @@ var port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 
+//add user specific routes
+require('./routes/user.js')(app);
+
 var options = {
   root: __dirname + '/public',
   dotfiles: 'deny',
@@ -14,10 +17,6 @@ var options = {
     'x-sent': true
   }
 }
-
-app.get('/', function(req, res){
-  res.render('index');
-});
 
 app.listen(port);
 console.log('Listening on port ' + port);
