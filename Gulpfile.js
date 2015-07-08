6f84a113a68a013820d135d451edcdd39b6ed267
Billy Rukh <billyrukh@gmail.com>
2015-07-08 18:20:39 -0500

    File structure for React & Flux. Updated Gulp.
    + Created file struture for working with React & Flux
    + Updated Gulp file to use browserfy, reactify, & vinyl-source-stream
    + Included browserfy task to bundle required modules
    + Included copy task for copying into our dist folder
    + Added a watcher for our client directory

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,11 @@ var config = require('./server/config/config');
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var bs = require('browser-sync');
+var browserify = require("browserify"); // require modules within the browser
+var reactify = require("reactify"); // converts JSX to JS
+var source = require("vinyl-source-stream"); // github.com/hughsk/vinyl-source-stream
 var reload = bs.reload;
+
 
 gulp.task('start', function () {
   nodemon({
@@ -10,7 +14,26 @@ gulp.task('start', function () {
     ext: 'js html ejs jsx',
     env: { 'NODE_ENV': 'development' }
   })
-})
+});
 
-gulp.task('default', ['start']);
+/* Bundles required modules */
+gulp.task('browserify', function() {
+  browserify('./client/js/main.js')
+    .transform('reactify')
+    .bundle()
+    .pipe(source('main.js'))
+    .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('copy',function() {
+  gulp.src('client/index.html')
+    .pipe(gulp.dest('dist'));
+  gulp.src('client/assets/**/*.*')
+    .pipe(gulp.dest('dist/assets'));
+});
+
+
+gulp.task('default',['start','browserify', 'copy'], function() {
+  return gulp.watch('client/**/*.*', ['browserify', 'copy']); // this only watches client changes
+});
 
