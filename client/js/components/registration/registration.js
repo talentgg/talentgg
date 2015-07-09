76427d3db6cf461f00fa0ede98a51dc997036ee6
Ronnie Brown <ronnievoice@gmail.com>
2015-07-09 10:10:00 -0700

    finish registration form component

--- client/js/components/registration/registration.js
+++ client/js/components/registration/registration.js
@@ -1,30 +1,40 @@
 var React = require('react');
 
+var fieldValues = {
+  name: null,
+  email: null,
+  password: null
+}
+
 var Form = React.createClass({
   handleSubmit: function(e) {
     e.preventDefault();
-    var userName = React.findDOMNode(this.refs.name);
-    var userPassword = React.findDOMNode(this.refs.password);
-    var userEmail = React.findDOMNode(this.refs.email);
-    this.props.signUp(this.props);
-    userName.value = '';
-    userPassword.value = '';
-    userEmail.value = '';
 
+    var data = {
+      name: this.refs.name.getDOMENode().value,
+      email: this.refs.email.getDOMENode().value,
+      password: this.refs.password.getDOMENode().value
+    }
+    
+    this.props.signUp(data);
+    
   },
   render: function() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <label>Name</label>
-          <input placeholder="name" ref="name" />
-
-        <label>Password</label>
-          <input placeholder="enter password" ref="password" />
-
-        <label>Email</label>
-          <input placeholder="enter email" ref="email" />
-
-        <button>Register</button>
+        <li>
+          <label>Name</label>
+            <input placeholder="name" ref="name" />
+        </li>
+        <li>
+          <label>Email</label>
+            <input placeholder="enter email" ref="email" />
+        </li>
+        <li>
+          <label>Password</label>
+            <input placeholder="enter password" ref="password" />
+        </li>
+          <button>Register</button>
       </form>
     );
   }
@@ -35,7 +45,7 @@ var Registration = React.createClass({
     return {};
   },
   signUp: function() {
-
+    // todo: send payload to server or action
   },
   render: function() {
     return (
