64199f493993951c45ce4d407a88eaa49fd74467
Ronnie Brown <ronnievoice@gmail.com>
2015-07-08 19:58:21 -0700

    add form component to registration

--- client/js/components/registration/registration.js
+++ client/js/components/registration/registration.js
@@ -1,10 +1,47 @@
-var RegistrationForm = React.createClass({
-  getInitialState: function() {
-    
+var React = require('react');
+
+var Form = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var userName = React.findDOMNode(this.refs.name);
+    var userPassword = React.findDOMNode(this.refs.password);
+    var userEmail = React.findDOMNode(this.refs.email);
+    this.props.signUp(this.props);
+    userName.value = '';
+    userPassword.value = '';
+    userEmail.value = '';
+
   },
   render: function() {
     return (
+      <form onSubmit={this.handleSubmit}>
+        <label>Name</label>
+          <input placeholder="name" ref="name" />
 
+        <label>Password</label>
+          <input placeholder="enter password" ref="password" />
+
+        <label>Email</label>
+          <input placeholder="enter email" ref="email" />
+
+        <button>Register</button>
+      </form>
     );
   }
 });
+
+var Registration = React.createClass({
+  getInitialState: function() {
+    return {};
+  },
+  signUp: function() {
+
+  },
+  render: function() {
+    return (
+      <Form />
+    );
+  }
+});
+
+module.exports = Registration;
