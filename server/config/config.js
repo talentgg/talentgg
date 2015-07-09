91781918096807c82b8bc6013317bd383e5b02d7
bryan <bry_angelo@yahoo.com>
2015-07-09 14:08:06 -0700

    api key

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000, // the server port
   psql: process.env.PSQL || 'postgres://localhost:5432/talentgg'
+  api: "c648b154-e354-43a5-9e56-7fe6eb1569ba"
 };
