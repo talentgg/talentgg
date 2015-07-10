ad2ec537e18841ad00dea9594a70f4123ab0e3d5
Ronnie Brown <ronnievoice@gmail.com>
2015-07-10 12:46:38 -0700

    stubs and user profile layout

---
+++ client/js/components/user-profile/avatar.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var avatar = React.createClass({
+  render: function() {
+    return: null
+  }
+});
+
+module.exports = Avatar;
---
+++ client/js/components/user-profile/footer.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var Footer = React.createClass({
+  render: function() {
+    return: null
+  }
+});
+
+module.exports = Footer;
