96a64e5339948e305b689ee0814b4af684be0cd5
Sean Grogg <sean.grogg@gmail.com>
2015-07-09 18:17:29 -0700

    fixed multiple areas; dependencies, schema, legacy code, etc

--- server/server.js
+++ server/server.js
@@ -1,22 +1,14 @@
-var express = require('express');
-var path = require('path');
+var express = require( 'express' );
+var path = require( 'path' );
 var app = express();
 var port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname, "views"));
-app.set('view engine', 'ejs');
+app.set( 'views', path.join( __dirname, "views" ) );
+app.set( 'view engine', 'ejs' );
 
 //add user specific routes
-require('./routes/user.js')(app);
+require( './routes/userroutes.js' )( app );
+// require( './routes/teamroutes.js' )( app );
 
-var options = {
-  root: __dirname + '/public',
-  dotfiles: 'deny',
-  headers: {
-    'x-timestamp': Date.now(),
-    'x-sent': true
-  }
-}
-
-app.listen(port);
-console.log('Listening on port ' + port);
+app.listen( port );
+console.log( 'Listening on port ' + port );
