6da619542b3de351b8f65d7952f7fae307b8deba
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-09 19:26:20 -0700

    question schema, profile directory

---
+++ server/models/questionModel.js
@@ -0,0 +1,26 @@
+var Sequelize = require( 'sequelize' );
+var DataTypes = require( '../../node_modules/sequelize/lib/data-types' );
+var PassportLocalStrategy = require( 'passport-local' ).Strategy;
+var db = require( '../db/db' );
+
+var Question = db.define( 'question', {
+
+  //Defaulted Fields
+  id: {
+    type: DataTypes.UUID,
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  lastUpdated: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+
+  //Static Fields
+  questionText: Sequelize.STRING,
+  category: Sequelize.STRING,  
+  answersText: Sequelize.ARRAY,
+  answersEffect: Sequelize.ARRAY,
+} );
+
+Team.sync();
