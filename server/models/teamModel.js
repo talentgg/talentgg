e3399a4d6c0c5ebf1e6de4370de66d3d762a19b2
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-09 18:48:00 -0700

    schema adjustments

---
+++ server/models/teamModel.js
@@ -0,0 +1,40 @@
+// -- deal with teams later
+
+// var Sequelize = require('sequelize');
+// var pg = require('pg').native;
+// var PassportLocalStrategy = require('passport-local').Strategy;
+
+// var Team = sequelize.define('team', {
+
+//   //FIXED FIELDS
+//   teamname: Sequelize.STRING(16),    
+//   timezone: Sequelize.INTEGER,// +- GMT
+//   language: Sequelize.STRING,
+//   game: Sequelize.STRING,
+//   seeking: Sequelize.STRING,
+//   lastUpdated: Sequelize.NOW,
+//   id: Sequelize.UUID,
+
+//   // ESSAY FIELDS
+//   who: Sequelize.STRING(1024),
+//   style: Sequelize.STRING(1024),
+//   when: Sequelize.STRING(1024),
+//   lookingFor: Sequelize.STRING(1024)
+
+//   // LoL specific                        // it'll just display the team members
+
+//   // // PROFILE RATINGS
+//   // //leadership
+//   // dominance: Sequelize.INTEGER,
+//   // adaptability: Sequelize.INTEGER,
+//   // //playing style
+//   // aggressive: Sequelize.INTEGER,      // aggressive / patient
+//   // tank: Sequelize.INTEGER,            // tank / technical
+//   // support: Sequelize.INTEGER,         // supportive / independent
+//   // troll: Sequelize.INTEGER,           // troll / polite
+//   // loud: Sequelize.INTEGER,            // loud / calm
+//   // commitment: Sequelize.INTEGER,
+//   // ambition: Sequelize.INTEGER
+// });
+
+// Team.sync();
