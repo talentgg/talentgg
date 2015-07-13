f3995be01aad669aa5286486678aa2b259c8cddd
Billy Rukh <billyrukh@gmail.com>
2015-07-13 15:22:34 -0500

    Updated dispatcher semantics
    + Payloads are now dispatched depending if it is a view action or a server action

--- client/js/dispatchers/app-dispatcher.js
+++ client/js/dispatchers/app-dispatcher.js
@@ -1,14 +1,27 @@
+/*
+  * OUR DISPATCHER HANDLES TWO TYPES OF REQUESTS. VIEWACTION REQUESTS WHICH ARE TRIGGERED BY OUR
+  * COMPONENTS. AND SERVER REQUESTS WHICH SHOULD BE TRIGGERED WITHIN OUR WEB API UTILITY
+  * BUT CAN ALSO BE TRIGGERED FROM A VIEW. THAT WOULD BE A ANTI-PATTERN SO WE SHOULD TRY TO
+  * AVOID THAT.
+  *
+  * IT THEN TAKES THAT ACTION'S PAYLOAD, CREATES A NEW OBJECT & DISPATCHES THE PAYLOAD TO ANY STORE
+  * THAT HAS IT REGISTERED.
+  *
+  * */
+
+
+
 var AppConstants = require("../constants/app-constants");
 var Dispatcher = require("flux").Dispatcher;
 var assign = require("object-assign");
 
-var PayloadSources = AppConstants.PayloadSources;
+var PayLoadSources = AppConstants.PayLoadSources;
 
 var AppDispatcher = assign(new Dispatcher(), {
 
   handleServerAction: function(action) {
     var payload = {
-      source: PayloadSources.SERVER_ACTION,
+      source: PayLoadSources.SERVER_ACTION,
       action: action
     };
     this.dispatch(payload);
@@ -16,7 +29,7 @@ var AppDispatcher = assign(new Dispatcher(), {
 
   handleViewAction: function(action) {
     var payload = {
-      source: PayloadSources.VIEW_ACTION,
+      source: PayLoadSources.VIEW_ACTION,
       action: action
     };
     this.dispatch(payload);
