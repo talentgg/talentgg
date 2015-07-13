9936af7c61f4b4c6a542b8e58b0ccbe61c4f835b
Billy Rukh <billyrukh@gmail.com>
2015-07-13 15:18:39 -0500

    Created actions for our server & sessions to handle

--- client/js/actions/ServerActionCreators.js
+++ client/js/actions/ServerActionCreators.js
@@ -1,3 +1,11 @@
+/* HANDLES ACTIONS THAT DEAL WITH SERVER REQUESTS
+ * RECEIVE LOGIN IS CALLED & TRIGGERED FROM WITHIN OUR
+ * WEB API UTILITY. THIS IS THEN DISPATCHED TO THE SESSIONSTORE
+ * WITH THE PAYLOAD
+ *
+ * */
+
+
 var AppDispatcher = require("../dispatchers/app-dispatcher");
 var AppConstants = require("../constants/app-constants");
 
@@ -8,7 +16,7 @@ module.exports = {
 
   recieveLogin: function(json, errors) {
     AppDispatcher.handleServerAction({
-      type: ActionTypes.LOGIN_RESPONSE,
+      actionType: ActionTypes.LOGIN_RESPONSE,
       json: json,
       errors: errors
     });
