ae337f995cd01726b5d761aad58303f79176b2f6
Billy Rukh <billyrukh@gmail.com>
2015-07-13 15:23:28 -0500

    Removed routes for readability and structure
    Added routes.js to store all our routes

--- client/js/main.js
+++ client/js/main.js
@@ -1,25 +1,11 @@
+/* THIS IS WHERE OUR INDEX LOOKS TO MOUNT OUR APP.
+ * WE ALSO PROVIDE ALL THE ROUTES USING REACT ROUTER  */
+
 var React = require('react');
-var router = require('./stores/RouteStore.react.jsx').getRouter();
+var router = require('./stores/RouteStore').getRouter();
 window.React = React;
 
 router.run(function (Handler, state) {
   React.render(<Handler/>, document.getElementById('content'));
 });
 
-
-/*
-  RIGHT NOW LANDING SHOWS UP WITH THE TEMPLATE, HERE IS HOW WE WILL FIX IT WITH AUTH ROUTES
- <Route handler={Application}>
-   <Route handler={Authed}> // VIEWS NESTED WHEN THEY ARE AUTHENTICATED
-     <Route name="dashboard" handler={Dashboard}/>
-     < name="profile" handler={Profile}/>
-   </Route>
-   <Route handler={NotAuthed}> // VIEWS WHEN THEY ARE NOT AUTHENTICATED
-     <Route name="login" handler={Login}/>
-     <Route name="about" handler={About}/>
-   </Route>
- </Route>
-
-
-
- */
