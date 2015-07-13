d90861a0ef73359a826ca4501132ad1719a9f97a
Billy Rukh <billyrukh@gmail.com>
2015-07-13 10:42:25 -0500

    Removed routes from main.js
    + Removed the routes from main.js into routes.js

--- client/js/main.js
+++ client/js/main.js
@@ -1,28 +1,11 @@
-var App = require("./components/app.js");
-var React = require("react");
-var Router = require('react-router');
-var Route = Router.Route;
-var RouteHandler = Router.RouteHandler;
-var Link = Router.Link;
+var React = require('react');
+var router = require('./stores/RouteStore.react.jsx').getRouter();
+window.React = React;
 
-
-var Registration = require("./components/registration/registration");
-var About = require("./components/about/app-about");
-var Landing = require("./components/landing/app-landing");
-
-APP = require('./components/app').APP;
-
-var routes = (
-  <Route handler={APP}>
-    <Route name="register" handler={Registration}/>
-    <Route name="landing" handler={Landing}/>
-    <Route name="about" handler={About}/>
-  </Route>
-);
-
-Router.run(routes, function (Handler) {
-  React.render(<Handler/>, document.getElementById('root'));
+router.run(function (Handler, state) {
+  React.render(<Handler/>, document.getElementById('content'));
 });
+
 
 /*
   RIGHT NOW LANDING SHOWS UP WITH THE TEMPLATE, HERE IS HOW WE WILL FIX IT WITH AUTH ROUTES
