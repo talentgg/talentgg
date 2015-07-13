80e3ff69ed35324699a69bf4453b0d88aa0d63a6
bryan <bry_angelo@yahoo.com>
2015-07-13 14:24:32 -0700

    added team oriented routes and helper functions

--- server/controllers/team.controller.js
+++ server/controllers/team.controller.js
@@ -1,1 +1,24 @@
+var User = require( '../models/team.model' );
+var passport = require( 'passport' );
+
+module.exports = {
+
+  getProfile: function( req, res, next ){
+    Team.findOne({where: {teamName: req.url.split('/')[2]}})
+      .then(function (teamProfile) {
+        return teamProfile;
+    })
+  },
+
+  updateProfile: function( req, res, next ){
+    User.update( {
+        activationKey: req.body.password
+      }, {
+        where: {
+          username: req.body.username
+        }
+      } )
+  }
+
+}
 
--- server/routes/team.routes.js
+++ server/routes/team.routes.js
@@ -1,1 +1,21 @@
+var Team = require( '../controllers/team.controller.js' );
+var passport = require( 'passport' );
+var handle = require( './handler' );
+var db = require( '../config/db.js' );
+
 // team routes here
+module.exports = function( app ) {
+
+  // get team profile
+  app.get( '/team/profile/*', function( req, res ) {
+    var profile = Team.getProfile(req);
+    res.send( profile );
+  } );
+
+  //update team profile
+  app.post( '/team/update/*', function( req, res ) {
+    Team.updateProfile(req);
+  } );
+
+  
+};
