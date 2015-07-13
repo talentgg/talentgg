af98b847e8d176d00b6809eb5fad48b3c974fbdb
Billy Rukh <billyrukh@gmail.com>
2015-07-13 15:24:34 -0500

    Added a WebAPI Utility
    + We can interface with API through this file
    + This way we only have data coming in through one source

--- client/js/utils/WebAPIUtils.js
+++ client/js/utils/WebAPIUtils.js
@@ -1,33 +1,70 @@
+/*
+  THIS IS WHERE WE TALK TO OUR BACKEND OR
+  ANY OTHER API INTERFACE.
+
+  USING SUPER AGENT AS OUR HTTP MODULE
+
+ */
 var ServerActionCreators = require("../actions/ServerActionCreators");
+var AppConstants = require("../constants/app-constants");
 var request = require("superagent");
 
 
-/* API ENDPOINTS */
+var APIEndpoints = AppConstants.APIEndPoints;
 
+
+function _getErrors(res) {
+  var errorMsgs = ["Something went wrong, please try again"];
+  if ((json = JSON.parse(res.text))) {
+    if (json['errors']) {
+      errorMsgs = json['errors'];
+    } else if (json['error']) {
+      errorMsgs = [json['error']];
+    }
+  }
+  return errorMsgs;
+}
 
 module.exports = {
 
-  signup: function (email, username,  password, passwordConfirmation) {
-
-
+  signup: function (email, username, password, passwordConfirmation) {
+    request.post(APIEndpoints.REGISTRATION)
+      .send({
+        user: {
+          email: email,
+          username: username,
+          password: password,
+          password_confirmation: passwordConfirmation
+        }
+      })
+      .set('Accept', 'application/json')
+      .end(function (error, res) {
+        if (res) {
+          if (res.error) {
+            var errorMsgs = _getErrors(res);
+            ServerActionCreators.receiveLogin(null, errorMsgs);
+          } else {
+            json = JSON.parse(res.text);
+            ServerActionCreators.receiveLogin(json, null);
+          }
+        }
+      });
   },
 
   login: function (email, password) {
-
-
-  },
-
-  getUserStats: function () {
-
-
-  },
-
-
-
+    request.post(APIEndpoints.LOGIN)
+      .send({email: email, password: password, grant_type: 'password'})
+      .set('Accept', 'application/json')
+      .end(function (error, res) {
+        if (res) {
+          if (res.error) {
+            var errorMsgs = _getErrors(res);
+            ServerActionCreators.receiveLogin(null, errorMsgs);
+          } else {
+            json = JSON.parse(res.text);
+            ServerActionCreators.receiveLogin(json, null);
+          }
+        }
+      });
+  }
 };
-
-
-/*    THIS IS WHERE WE TALK TO OUR BACKEND OR
-      ANY OTHER API INTERFACE.
-
- */
