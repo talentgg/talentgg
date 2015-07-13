0e4af4e945f3bd2f799940bbc7416899fe7f96b2
Billy Rukh <billyrukh@gmail.com>
2015-07-13 10:51:24 -0500

    Created as a utility to manage Web API's
    + This file will help us interaface with different API's

---
+++ client/js/utils/WebAPIUtils.js
@@ -0,0 +1,33 @@
+var ServerActionCreators = require("../actions/ServerActionCreators");
+var request = require("superagent");
+
+
+/* API ENDPOINTS */
+
+
+module.exports = {
+
+  signup: function (email, username,  password, passwordConfirmation) {
+
+
+  },
+
+  login: function (email, password) {
+
+
+  },
+
+  getUserStats: function () {
+
+
+  },
+
+
+
+};
+
+
+/*    THIS IS WHERE WE TALK TO OUR BACKEND OR
+      ANY OTHER API INTERFACE.
+
+ */
