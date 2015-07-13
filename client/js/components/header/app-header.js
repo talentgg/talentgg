0952aa7475241699024e5b80fff504a9e204b36c
Billy Rukh <billyrukh@gmail.com>
2015-07-13 15:21:17 -0500

    Updated header and session components

--- client/js/components/header/app-header.js
+++ client/js/components/header/app-header.js
@@ -1,19 +1,54 @@
-var React = require("react");
-var Router = require('react-router');
-var Route = Router.Route;
-var RouteHandler = Router.RouteHandler;
-var Link = Router.Link;
+/* OUR HEADER INHERITS PROPS FROM OUR MAIN APP COMPONENT
+ * THIS LETS IT ACCESS THE SESSIONSTORE TO CHECK IF SOMEONE IS LOGGED IN.
+ * IF THEY ARE NOT LOGGED IN WE DISPLAY LOGIN OR SIGNUP.
+ * IF THEY ARE LOGGED IN WE WOULD DISPLAY SOMETHING CUSTOM,
+ * HAVE A PLACE HOLDER CURRENTLY
+ */
 
+var React = require('react');
+var Router = require('react-router');
+var Link = Router.Link;
+var ReactPropTypes = React.PropTypes;
+var SessionActionCreators = require('../../actions/SessionActionCreators');
 
 var Header = React.createClass({
-  render: function () {
+
+  propTypes: {
+    isLoggedIn: ReactPropTypes.bool,
+    email: ReactPropTypes.string
+  },
+  logout: function(e) {
+    e.preventDefault();
+    SessionActionCreators.logout();
+  },
+  render: function() {
+    /*var rightNav = this.props.isLoggedIn ? (
+
+      /!* WHAT TO DISPLAY IN THE HEADER IF THEY ARE LOGGED IN *!/
+
+      <ul>
+        <li><Link to="login">Login</Link></li>
+        <li><Link to="signup">Sign up</Link></li>
+      </ul>
+
+    ) : (
+
+      /!* WHAT TO DISPLAY IF THEY ARENT LOGGED IN (PLACEHOLDER) *!/
+
+      <ul>
+        <li><Link to="login">Login</Link></li>
+        <li><Link to="signup">Sign up</Link></li>
+      </ul>
+    );*/
+
     return (
       <div>
+
         <Link to="/"><h1>Talent.gg</h1></Link>
 
-        /* add navigation */
-        <Link to="register">Register</Link>
-        <Link to="about">About</Link>
+        <li><Link to="login">Login</Link></li>
+        <li><Link to="signup">Sign up</Link></li>
+
       </div>
     );
   }
