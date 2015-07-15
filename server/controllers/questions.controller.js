673f0971f1813f1ba87493511d0e8dac56a40a72
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-14 20:38:18 -0700

    q's record and display answer history

--- server/controllers/questions.controller.js
+++ server/controllers/questions.controller.js
@@ -5,58 +5,52 @@ module.exports = {
 
   //create a new User
   getAllQuestions: function(req, res, next) {
-    // console.log(req.body);
-    // Question.findAll()
-    //     .then(function() {
+    
+    // these are arrays... do we even need value?
+
     var questions = [{
-      questionText: "How do you want to change testprofile1?",
-      answers: [{
-        label: "add 10 to attribute1",
-        value: 1,
-        effects: [10],
-        categories: ["attribute1"]
-      }, {
-        label: "subtract 10 from attribute1",
-        value: 2,
-        effects: [-10],
-        categories: ["attribute1"]
-      }, {
-        label: "change attribute2 -10",
-        value: 3,
-        effects: [-10],
-        categories: ["attribute2"]
-      }, {
-        label: "change attribute5 -5 and attribute3 -7",
-        value: 4,
-        effects: [-5, -7],
-        categories: ["attribute5", "attribute3"]
-      }],
-      potential: []
-    }, {
-      questionText: "Does this work?",
-      answers: [{
-        label: "yes",
-        value: 1,
-        effects: [10],
-        categories: ["attribute1"]
-      }, {
-        label: "no",
-        value: 2,
-        effects: [-10],
-        categories: ["attribute1"]
-      }, {
-        label: "change attribute2 -10",
-        value: 3,
-        effects: [-10],
-        categories: ["attribute2"]
-      }, {
-        label: "change attribute5 -5 and attribute3 -7",
-        value: 4,
-        effects: [-5, -7],
-        categories: ["attribute5", "attribute3"]
-      }],
-      potential: []
-    }];
+        questionText: "How do you want to change testprofile1?",
+        answers: [{label: "add 10 to attribute1", value: 0, effects: [10], categories: ["attribute1"]},
+        {label: "subtract 10 from attribute1", value: 1, effects: [-10], categories: ["attribute1"]},
+        {label: "change attribute2 -10", value: 2, effects: [-10], categories: ["attribute2"]},
+        {label: "change attribute5 -5 and attribute3 -7", value: 3, effects: [-5, -7], categories: ["attribute5", "attribute3"]}],
+        potential: []
+      },
+      {
+        questionText: "Does this work?",
+        answers: [{label: "yes", value: 0, effects: [10], categories: ["attribute1"]},
+        {label: "no", value: 1, effects: [-10], categories: ["attribute1"]},
+        {label: "change attribute2 -10", value: 2, effects: [-10], categories: ["attribute2"]},
+        {label: "change attribute5 -5 and attribute3 -7", value: 3, effects: [-5, -7], categories: ["attribute5", "attribute3"]}],
+        potential: []
+      },
+            {
+        questionText: "Does this also work?",
+        answers: [{label: "yes", value: 0, effects: [10], categories: ["attribute1"]},
+        {label: "no", value: 1, effects: [-10], categories: ["attribute1"]},
+        {label: "change attribute2 -10", value: 2, effects: [-10], categories: ["attribute2"]},
+        {label: "change attribute5 -5 and attribute3 -7", value: 3, effects: [-5, -7], categories: ["attribute5", "attribute3"]}],
+        potential: []
+      },
+            {
+        questionText: "Does this work too?",
+        answers: [{label: "yes", value: 0, effects: [10], categories: ["attribute1"]},
+        {label: "no", value: 1, effects: [-10], categories: ["attribute1"]},
+        {label: "change attribute2 -10", value: 2, effects: [-10], categories: ["attribute2"]},
+        {label: "change attribute5 -5 and attribute3 -7", value: 3, effects: [-5, -7], categories: ["attribute5", "attribute3"]}],
+        potential: []
+      },
+            {
+        questionText: "Does this also work too?",
+        answers: [{label: "yes", value: 0, effects: [10], categories: ["attribute1"]},
+        {label: "no", value: 1, effects: [-10], categories: ["attribute1"]},
+        {label: "change attribute2 -10", value: 2, effects: [-10], categories: ["attribute2"]},
+        {label: "change attribute5 -5 and attribute3 -7", value: 3, effects: [-5, -7], categories: ["attribute5", "attribute3"]}],
+        potential: []
+      }
+
+      ];
+    
 
     return questions;
     // });
