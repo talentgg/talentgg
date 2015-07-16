e33c7d6d63782bea5411d3da66e81068df549a3b
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-16 14:32:14 -0700

    update for q, attribute integration

--- server/data/questions.js
+++ server/data/questions.js
@@ -1,10 +1,16 @@
 var testData = [{
-        questionText: "How do you want to change testprofile1?",
-        answers: [{label: "add 10 to attribute1", value: 0, effects: [10], categories: ["attribute1"]},
-        {label: "subtract 10 from attribute1", value: 1, effects: [-10], categories: ["attribute1"]},
-        {label: "change attribute2 -10", value: 2, effects: [-10], categories: ["attribute2"]},
-        {label: "change attribute5 -5 and attribute3 -7", value: 3, effects: [-5, -7], categories: ["attribute5", "attribute3"]}],
-        potential: []
+        questionText: "While taking on a good team, it becomes clear that one of your teammates is by far the weakest player in the game. Your opponents have noticed and are successfully exploiting his lane. Which of these is most similar to your response?",
+        answers: [
+        {label: "Make it clear that the player should find a new team for the next match, and get their head out of their ass for this one.", value: 0, effects: [10, 10, 2, -2, 10], categories: ["troll", "loud", "ambition", "adaptable", "dominance"]},
+        {label: "Direct the player on how to play better.", value: 1, effects: [5, 1, 5, 2], categories: ["dominance", "adaptable", "blunt", "ambition"]},
+        {label: "Direct your team to compensate for your weak teammate.", value: 2, effects: [5, 3, 5], categories: ["dominance", "adaptable", "collaborative"]},
+        {label: "Keep an eye on their area of the board, and hop over whenever need be.", value: 3, effects: [5, 2, -10, -10, -5, -5], categories: ["adaptable", "collaborative", "loud", "dominance", "blunt", "troll"]},
+        {label: "Stay in your lane. If the other team is focused on your teammate, they'll be too distracted to deal with your plans.", value: 4, effects: [5, -5, -5, -10, -5, -3], categories: ["adaptable", "blunt", "collaborative", "loud", "dominance", "troll"]}
+        ],
+        potential: [{
+            categories: ["troll", "loud", "dominance", "adaptable", "collaborative"],
+            values: [10, 10, 10, 5, 5]
+        }]
       },
       {
         questionText: "Does this work?",
