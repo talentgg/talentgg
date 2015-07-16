15aa8b2ac28283642c150247554edd5c38b16666
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-16 15:23:31 -0700

    starting user profile integration

--- server/data/questions.js
+++ server/data/questions.js
@@ -1,17 +1,18 @@
-var testData = [{
-        questionText: "While taking on a good team, it becomes clear that one of your teammates is by far the weakest player in the game. Your opponents have noticed and are successfully exploiting his lane. Which of these is most similar to your response?",
-        answers: [
-        {label: "Make it clear that the player should find a new team for the next match, and get their head out of their ass for this one.", value: 0, effects: [10, 10, 2, -2, 10], categories: ["troll", "loud", "ambition", "adaptable", "dominance"]},
-        {label: "Direct the player on how to play better.", value: 1, effects: [5, 1, 5, 2], categories: ["dominance", "adaptable", "blunt", "ambition"]},
-        {label: "Direct your team to compensate for your weak teammate.", value: 2, effects: [5, 3, 5], categories: ["dominance", "adaptable", "collaborative"]},
-        {label: "Keep an eye on their area of the board, and hop over whenever need be.", value: 3, effects: [5, 2, -10, -10, -5, -5], categories: ["adaptable", "collaborative", "loud", "dominance", "blunt", "troll"]},
-        {label: "Stay in your lane. If the other team is focused on your teammate, they'll be too distracted to deal with your plans.", value: 4, effects: [5, -5, -5, -10, -5, -3], categories: ["adaptable", "blunt", "collaborative", "loud", "dominance", "troll"]}
-        ],
-        potential: [{
-            categories: ["troll", "loud", "dominance", "adaptable", "collaborative"],
-            values: [10, 10, 10, 5, 5]
-        }]
-      },
+var testData = [
+// {
+//         questionText: "While taking on a good team, it becomes clear that one of your teammates is by far the weakest player in the game. Your opponents have noticed and are successfully exploiting his lane. Which of these is most similar to your response?",
+//         answers: [
+//         {label: "Make it clear that the player should find a new team for the next match, and get their head out of their ass for this one.", value: 0, effects: [10, 10, 2, -2, 10], categories: ["troll", "loud", "ambition", "adaptable", "dominance"]},
+//         {label: "Direct the player on how to play better.", value: 1, effects: [5, 1, 5, 2], categories: ["dominance", "adaptable", "blunt", "ambition"]},
+//         {label: "Direct your team to compensate for your weak teammate.", value: 2, effects: [5, 3, 5], categories: ["dominance", "adaptable", "collaborative"]},
+//         {label: "Keep an eye on their area of the board, and hop over whenever need be.", value: 3, effects: [5, 2, -10, -10, -5, -5], categories: ["adaptable", "collaborative", "loud", "dominance", "blunt", "troll"]},
+//         {label: "Stay in your lane. If the other team is focused on your teammate, they'll be too distracted to deal with your plans.", value: 4, effects: [5, -5, -5, -10, -5, -3], categories: ["adaptable", "blunt", "collaborative", "loud", "dominance", "troll"]}
+//         ],
+//         potential: [{
+//             categories: ["troll", "loud", "dominance", "adaptable", "collaborative"],
+//             values: [10, 10, 10, 5, 5]
+//         }]
+//       },
       {
         questionText: "Does this work?",
         answers: [{label: "yes", value: 0, effects: [10], categories: ["attribute1"]},
