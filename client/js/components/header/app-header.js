e775440722136498339d44dacd7c89506dd39392
bryan <bry_angelo@yahoo.com>
2015-07-16 19:21:44 -0700

    h

--- client/js/components/header/app-header.js
+++ client/js/components/header/app-header.js
@@ -11,11 +11,17 @@ var Link = Router.Link;
 var ReactPropTypes = React.PropTypes;
 var SessionActionCreators = require('../../actions/SessionActionCreators');
 
+var list = this;
+console.log('this is the state' + JSON.stringify(list));
+
 var Header = React.createClass({
+
+  teams: ['teamA', 'teamB', 'teamC'],
 
   propTypes: {
     isLoggedIn: ReactPropTypes.bool,
-    email: ReactPropTypes.string
+    email: ReactPropTypes.string,
+    teams: ReactPropTypes.array
   },
   logout: function(e) {
     e.preventDefault();
@@ -38,6 +44,17 @@ var Header = React.createClass({
               {/* I recommend this side be used for notifications and self-referentials (my profile, my settings) */}
               <li><Link to="/profile"><span className="glyphicon glyphicon-envelope"></span></Link></li>
               <li><Link to="/profile"><span className="glyphicon glyphicon-user"></span> Username</Link></li>
+              <li><Link to="/profile"><span className="glyphicon glyphicon-user"></span> My Profile</Link></li>
+              <li className="dropdown">
+                <Link to='#' className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></Link>
+                <ul className="dropdown-menu">
+                  <li><Link to="/">Action</Link></li>
+                  <li><Link to="/">Another action</Link></li>
+                  <li><Link to="/">Something else here</Link></li>
+                </ul>
+              </li>
+              <li><Link to="/profile"><span className="glyphicon glyphicon-search"></span> Recruitment </Link></li>
+              <li><Link to="/profile"><span className="glyphicon glyphicon-off"></span> Sign Out</Link></li>
             </ul>
           </div>
         </div>
