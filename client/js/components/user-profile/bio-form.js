cdae411a1db38c629b70c3ffb51b7d2d2f5f12e5
Ronnie Brown <ronnievoice@gmail.com>
2015-07-17 16:49:58 -0700

    add new questions to bio-form adjust layout for bio added newlines

--- client/js/components/user-profile/bio-form.js
+++ client/js/components/user-profile/bio-form.js
@@ -7,22 +7,20 @@ var BioForm = React.createClass({
   propTypes: {
     username: React.PropTypes.string.isRequired,
   },
-  // handleSubmit: function() {
-  //   // todo: remove jenk
-  //   var router = this.context.router;
-  //   setTimeout(function() {
-  //     router.transitionTo('profile', {username: 'username'});
-  //   }, 100);
-  // },
   render: function() {
     return (
       <div className="BioForm">
         <form method="POST" action="/profile" >
           <li>
-            <label>Times Available</label>
-            <input name="times" placeholder="edit times" ref="times" />
+            <label>Times Available:</label>
+            <select name="times">
+              <option value="weekdays" selected>Weekdays</option>
+              <option value="weeknights">Weeknights</option>
+              <option value="weekends">Weekends</option>
+            </select> 
           </li>
           <li>
+            <label>Seeking:</label>
             <select name="seeking">
               <option value="seeking" selected>Seeking</option>
               <option value="recruiting">Recruiting</option>
@@ -30,24 +28,50 @@ var BioForm = React.createClass({
             </select> 
           </li>
           <li>
-            <label>About</label>
-            <textarea name="about" placeholder="edit about" ref="about"></textarea>
+            <label>Purpose:</label>
+            <select name="purpose">
+              <option value="2v2 ranked" selected>2v2 Ranked</option>
+              <option value="3v3 casual">3v3 Casual</option>
+              <option value="5v5 casual">5v5 Casual</option>
+              <option value="5v5 ranked">5v5 Ranked</option>
+            </select> 
           </li>
           <li>
-            <label>Fav</label>
-            <input name="fav" placeholder="edit fav games" ref="fav" />
+            <label>Region:</label>
+            <select name="region">
+              <option value="NA" selected>NA</option>
+              <option value="EU">EU</option>
+            </select>
           </li>
           <li>
-            <label>Looking</label>
-            <input name="looking" placeholder="edit looking for" ref="looking" />
+            <label>Will Do:</label>
+              Tank <input type="checkbox" name="willdo" value="tank " /> 
+              Jungle <input type="checkbox" name="willdo" value="jungle " /> 
+              Support <input type="checkbox" name="willdo" value="support " /> 
+              Mid <input type="checkbox" name="willdo" value="mid " /> 
+              ADC <input type="checkbox" name="willdo" value="adc " /> 
+              Fill <input type="checkbox" name="willdo" value="fill " />
           </li>
           <li>
-            <label>Summoner id</label>
+            <label>Wont Do:</label>
+              Tank <input type="checkbox" name="wontdo" value="tank " /> 
+              Jungle <input type="checkbox" name="wontdo" value="jungle " /> 
+              Support <input type="checkbox" name="wontdo" value="support " /> 
+              Mid <input type="checkbox" name="wontdo" value="mid " /> 
+              ADC <input type="checkbox" name="wontdo" value="adc " /> 
+              Fill <input type="checkbox" name="wontdo" value="fill " />
+          </li>
+          <li>
+            <label>Summoner id:</label>
             <input name="summoner" placeholder="edit summoner" ref="summoner" />
           </li>
           <li>
-            <label>Region</label>
-            <input name="region" placeholder="edit region" ref="region" />
+            <label>About Me:</label>
+            <textarea name="about" placeholder="edit about" ref="about"></textarea>
+          </li>
+          <li>
+            <label>Favorite Games:</label>
+            <input name="favorite" placeholder="edit favorite games" ref="fav" />
           </li>
           <button type="submit" className="btn btn-sml">Joe Budden</button>
         </form>
