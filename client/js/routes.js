caa4ddac7c0249658bfdf66ac64857f640175f27
Billy Rukh <billyrukh@gmail.com>
2015-07-17 19:58:08 -0500

    Routes for notfound & default routes

--- client/js/routes.js
+++ client/js/routes.js
@@ -9,6 +9,8 @@ var React = require("react");
 var Router = require("react-router");
 var Route = Router.Route;
 var DefaultRoute = Router.DefaultRoute;
+var NotFoundRoute = Router.NotFoundRoute;
+
 
 /* COMPONENTS TO RENDER DEPENDING ON THE REQUESTED ROUTE */
 
@@ -21,23 +23,22 @@ var Profile = require("./components/user-profile/profile");
 var UserQuestions = require("./components/user-profile/user-questions");
 var BioForm = require("./components/user-profile/bio-form");
 var Settings = require("./components/user-profile/settings");
+var NotFound = React.createClass({
+  render: function () {
+    return <h2>NOT FOUND BITCH</h2>;
+  }
+});
 
 module.exports = (
 
   <Route name="app" path="/" handler={App}>
+    <DefaultRoute handler={UserProfile} />
     <Route name="user-profile" path="/user-profile/:username" handler={UserProfile}/>
     <Route name="profile" path="/user-profile" handler={Profile}/>
     <Route name="userquestions" path="/userquestions" source="/questions" handler={UserQuestions}/>
     <Route name="bioform" path="/bioform" handler={BioForm}/>
     <Route name="settings" path="/settings" handler={Settings}/>
+    <NotFoundRoute handler={NotFound}/>
   </Route>
 
 );
-
-/*
-These routes no longer seem necessary given they're handled in a different context entirely
-
-<Route name="login" path="/login" handler={LoginPage}/>
-<Route name="signup" path="/signup" handler={SignupPage}/>
-<Route name="about" path="/about" handler={About}/>
-*/
