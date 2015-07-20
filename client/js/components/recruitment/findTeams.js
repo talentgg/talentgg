5327c7aa5752ce46fa88c6d4195b14db459f8bb7
bryan <bry_angelo@yahoo.com>
2015-07-20 15:29:28 -0700

    search filters / improved matches display

--- client/js/components/recruitment/findTeams.js
+++ client/js/components/recruitment/findTeams.js
@@ -1,6 +1,10 @@
 var React = require('react');
 var axios = require('axios');
 
+var whiteBox = {backgroundColor: 'white', padding: '25', margin:'25', border: 'solid black 2px', height: '250', width: '450', display: 'inline-block'};
+var headshot = {backgroundColor: 'white', padding: '10', border: 'solid red 2px', height: '200', width: '200', float: 'left', textAlign: 'center'};
+var stats = {backgroundColor: 'white', padding: '25', border: 'solid blue 2px', height: '200', width: '200', display: 'block', float: 'right', textAlign: 'center'};
+
 var FindTeams = React.createClass({
   getInitialState: function() {
     return {
@@ -40,9 +44,46 @@ var FindTeams = React.createClass({
   render: function() {
     return (      
       <div className="findTeams">
-        <p> I WORK! </p>
-
         <h1> Matches </h1>
+        <h2> Filters </h2>
+          <li>
+              <label>Times Available:</label>
+              <select className="form-control" name="times">
+                <option value="weekdays" selected>Weekdays</option>
+                <option value="weeknights">Weeknights</option>
+                <option value="weekends">Weekends</option>
+              </select>
+          </li>
+          <li>
+            <label>Purpose:</label>
+            <select className="form-control" name="purpose">
+              <option value="2v2 ranked" selected>2v2 Ranked</option>
+              <option value="3v3 casual">3v3 Casual</option>
+              <option value="5v5 casual">5v5 Casual</option>
+              <option value="5v5 ranked">5v5 Ranked</option>
+            </select> 
+          </li>
+          <li className="form-group checkbox inline no_indent">
+            <label>Will Do:</label>
+            <label className="checkbox inline no_indent">
+              <input type="checkbox" name="willdo" value="tank " />Tank
+            </label>  
+            <label className="checkbox inline no_indent">
+              <input type="checkbox" name="willdo" value=" jungle" />Jungle
+            </label>  
+            <label className="checkbox inline no_indent">
+              <input type="checkbox" name="willdo" value=" support" />Support
+            </label>
+            <label className="checkbox inline no_indent"> 
+              <input type="checkbox" name="willdo" value=" mid" />Mid
+            </label>
+            <label className="checkbox inline no_indent">  
+              <input type="checkbox" name="willdo" value=" adc" />ADC
+            </label>
+            <label className="checkbox inline no_indent">  
+              <input type="checkbox" name="willdo" value=" fill" />Fill
+            </label>
+          </li>
         <MatchList users={this.state.users} me={this.state.me} id={this.state.id} />    
       </div>
       );
@@ -75,14 +116,19 @@ var MatchList = React.createClass({
         };
         overallScore = Math.round(calculateMatchScore(overallScore, 200) * 100);
         MatchNodes.push(
-          <div>
-            <div> { this.props.users[i].displayName } </div>
-            <div> { this.props.users[i].bio.willdo } </div>
-            <div> { this.props.users[i].bio.purpose } </div>    
-            <div> { this.props.users[i].bio.times } </div>
-            <div> {overallScore}% </div>
-            <br />
-            <br />
+          <div className="row" style={whiteBox}>
+            <div className="row" style={headshot}>
+              <img className="img-circle center-block" src={this.props.users[i].games.avatar}/>
+              <div align="center"> { this.props.users[i].displayName } </div>
+              <div> {overallScore}% </div>
+            </div>
+            <div className="row" style={stats}>
+              <div> { this.props.users[i].bio.willdo } </div>
+              <div> { this.props.users[i].bio.purpose } </div>    
+              <div> { this.props.users[i].bio.times } </div>
+              <br />
+              <br />
+            </div>
           </div>)
         }
       }
