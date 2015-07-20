e8c91cdef496e13b26d83c69142fe3c0743f9b65
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-20 12:47:29 -0700

    deconsole logged

--- client/js/components/recruitment/findTeams.js
+++ client/js/components/recruitment/findTeams.js
@@ -27,9 +27,6 @@ var FindTeams = React.createClass({
 
     axios.all([getThem(), getMe()])
         .then(axios.spread(function(them, me) { 
-            console.log("-------");
-            console.log(them.data);
-            console.log("-------");
             context.setState({
               users: them.data,
               me: me.data.ratings,
@@ -104,12 +101,7 @@ var MatchList = React.createClass({
     var overallScore = 0;
     for (var i = 0; i < this.props.users.length; i++){
       if (this.props.users[i].id !== this.props.id) {
-        for (key in this.props.me) {
-          console.log("xxxxxxxx", i)
-          console.log(key);
-          console.log(this.props.me[key])
-          console.log(this.props.users[i].ratings[key])
-          console.log("xxxxxxxx", i)
+        for (key in this.props.me) {          
           var score = 20 - Math.abs(this.props.me[key] - this.props.users[i].ratings[key]);
           overallScore += score;
           score = calculateMatchScore(score, 20);        
