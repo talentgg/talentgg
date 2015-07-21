2c9c056cc4691ac6e2c4ddd9274b332adf1cd7be
jonathan seneris <jonathanpseneris@gmail.com>
2015-07-20 21:10:43 -0700

    added belle, react button

--- client/js/components/recruitment/findTeams.js
+++ client/js/components/recruitment/findTeams.js
@@ -1,9 +1,17 @@
 var React = require('react');
 var axios = require('axios');
 
+var ReactBtn = require('react-btn-checkbox');
+var Checkbox = ReactBtn.Checkbox;
+var Radio = ReactBtn.Radio;
+
+var belle = require('belle');
+Button = belle.Button;
+
 var whiteBox = {backgroundColor: 'white', padding: '25', margin:'25', border: 'solid black 2px', height: '250', width: '450', display: 'inline-block'};
 var headshot = {backgroundColor: 'white', padding: '10', border: 'solid red 2px', height: '200', width: '200', float: 'left', textAlign: 'center'};
 var stats = {backgroundColor: 'white', padding: '25', border: 'solid blue 2px', height: '200', width: '200', display: 'block', float: 'right', textAlign: 'center'};
+
 
 var FindTeams = React.createClass({
   getInitialState: function() {
@@ -12,7 +20,14 @@ var FindTeams = React.createClass({
       me: {},      
       times: "any",
       purpose: "any",
-      willdo: "any",
+      willdo: {
+        'Tank': false,
+        'Jungle': false,
+        'Support': false,
+        'Mid': false,
+        'ADC': false,
+        'Fill': true
+      },
       id: 0
     };
   },
@@ -35,14 +50,17 @@ var FindTeams = React.createClass({
               id: me.data.id             
             });
         }));
-  },     
+  },
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    for (key in this.state) {
+      console.log("key", key);
+      console.log(this.state[key]);
+    }
+  },
 
   handleChange: function(e) {
-    console.log(e.target);
-    console.log(e.target.value);
-    console.log(e.target.name);
-    console.log("change!");
-    
     switch (e.target.name) {
       case "times":
         this.setState({
@@ -56,31 +74,27 @@ var FindTeams = React.createClass({
         });
         break;
 
-      case "willdo":
-        this.setState({
-          willdo: e.target.value
-        });
-        break;
+      // case "willdo":
+      //   this.setState({
+      //     willdo: e.target.value
+      //   });
+      //   break;
 
       default:
         console.log(e.target.name);
         break;
     }
-
-    console.log(this.state.times);
-    console.log(this.state.purpose);
-    console.log(this.state.willdo);
-    console.log(this.state.willdo.state.check);
   },
   render: function() {
-    return (      
+
+    return (     
       <div className="findTeams">
         <h1> Matches </h1>
         <h2> Filters </h2>
           <li>
               <label>Times Available:</label>
               <select className="form-control" value={this.state.times} onChange={this.handleChange} name="times">
-                <option value="weekdays" selected>Weekdays</option>
+                <option value="weekdays" defaultValue>Weekdays</option>
                 <option value="weeknights">Weeknights</option>
                 <option value="weekends">Weekends</option>
               </select>
@@ -88,36 +102,24 @@ var FindTeams = React.createClass({
           <li>
             <label>Purpose:</label>
             <select className="form-control" value={this.state.purpose} onChange={this.handleChange}  name="purpose">
-              <option value="2v2 ranked" selected>2v2 Ranked</option>
+              <option value="2v2 ranked" defaultValue>2v2 Ranked</option>
               <option value="3v3 casual">3v3 Casual</option>
               <option value="5v5 casual">5v5 Casual</option>
               <option value="5v5 ranked">5v5 Ranked</option>
             </select> 
           </li>
 
-          <li className="form-group checkbox inline no_indent" value={this.state.willdo} onChange={this.handleChange} >
-            
-            <label>Will Do:</label>
-            <label className="checkbox inline no_indent">
-              <input type="checkbox" name="willdo" value="tank" ref="willdo" />Tank
-            </label>  
-            <label className="checkbox inline no_indent">
-              <input type="checkbox" name="willdo" value="jungle" ref="willdo"/>Jungle
-            </label>  
-            <label className="checkbox inline no_indent">
-              <input type="checkbox" name="willdo" value="support" ref="willdo"/>Support
-            </label>
-            <label className="checkbox inline no_indent"> 
-              <input type="checkbox" name="willdo" value="mid" ref="willdo"/>Mid
-            </label>
-            <label className="checkbox inline no_indent">  
-              <input type="checkbox" name="willdo" value="adc" ref="willdo"/>ADC
-            </label>
-            <label className="checkbox inline no_indent">  
-              <input type="checkbox" name="willdo" value="fill" ref="willdo"/>Fill
-            </label>
+          <form onSubmit={this.handleSubmit}>
+          
+            <Checkbox
+            label='Checkbox'
+            options={this.state.willdo}
+            onChange={this.setState.bind(this)} />
 
-          </li>
+            <Button primary type="submit" value="Submit">Submit</Button>
+          </form>
+
+          
 
 
         <MatchList users={this.state.users} me={this.state.me} id={this.state.id} filters={this.state.filters} />    
@@ -138,6 +140,44 @@ var MatchList = React.createClass({
     };
     var MatchNodes = [];
     var overallScore = 0;
+
+    var LinkedList = function() {
+      var list = {};
+      list.head = null;
+      list.tail = null;
+      list.addToTail = function(value) {
+          var newNode = new Node(value);
+          newNode.previous = list.tail;
+          list.tail = newNode;
+          if (list.head === null) {
+              list.head = newNode;
+          } else {
+              var current = list.head;
+              while (current.next) {
+                  current = current.next;
+              }
+              current.next = newNode;
+          }
+          list.counter++
+      };
+      
+      var Node = function(value) {
+          var node = {};
+          node.value = value;
+          node.next = null;
+          node.previous = null;
+          return node;
+      }
+    };
+
+    var filteredUsers = new LinkedList(this.props.users[0]);
+    while () {
+      if (this.state.times === "any" || this.state.times === this.props.users[i].bio.times) {
+        filteredUsers.addToTail(this.props.users[i]);
+      }
+    }
+
+
     for (var i = 0; i < this.props.users.length; i++){
       if (this.props.users[i].id !== this.props.id) {
         for (key in this.props.me) {          
@@ -174,3 +214,28 @@ var MatchList = React.createClass({
     );
   }
 });
+
+
+// <li className="form-group checkbox inline no_indent" value={this.state.willdo} onChange={this.handleChange} >
+            
+          //   <label>Will Do:</label>
+          //   <label className="checkbox inline no_indent">
+          //     <input type="checkbox" name="willdo" value="tank" ref="willdo" />Tank
+          //   </label>  
+          //   <label className="checkbox inline no_indent">
+          //     <input type="checkbox" name="willdo" value="jungle" ref="willdo"/>Jungle
+          //   </label>  
+          //   <label className="checkbox inline no_indent">
+          //     <input type="checkbox" name="willdo" value="support" ref="willdo"/>Support
+          //   </label>
+          //   <label className="checkbox inline no_indent"> 
+          //     <input type="checkbox" name="willdo" value="mid" ref="willdo"/>Mid
+          //   </label>
+          //   <label className="checkbox inline no_indent">  
+          //     <input type="checkbox" name="willdo" value="adc" ref="willdo"/>ADC
+          //   </label>
+          //   <label className="checkbox inline no_indent">  
+          //     <input type="checkbox" name="willdo" value="fill" ref="willdo"/>Fill
+          //   </label>
+
+          // </li>
