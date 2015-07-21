9859275e722e6a1785a10077261e9b857ac30df2
bryan <bry_angelo@yahoo.com>
2015-07-21 12:29:18 -0700

    team profile registration page added

---
+++ client/js/components/team/team-registration.js
@@ -0,0 +1,62 @@
+var React = require('react');
+var Router = require('react-router');
+var Axios = require('axios');
+
+var TeamRegistration = React.createClass({
+  mixins: [Router.State, Router.Navigation],
+  propTypes: {
+    teamName: React.PropTypes.string.isRequired,
+  },
+  getInitialState: function() {
+    return {};
+  },
+  render: function() {
+    return (
+      <div className="container">
+        <h1> Team Settings </h1>
+        <form method="POST" action="/team/register">
+          <li>
+            <label> Team Name: </label>
+            <input name="teamName" className="form-control" placeholder="enter a team name" type="text" />
+          </li>
+          <li>
+            <label>Times Available:</label>
+            <select className="form-control" name="times">
+              <option value="weekdays" selected>Weekdays</option>
+              <option value="weeknights">Weeknights</option>
+              <option value="weekends">Weekends</option>
+            </select> 
+          </li>
+          <li className="form-group checkbox inline no_indent">
+            <label> Looking For: </label>
+            <label className="checkbox inline no_indent">
+              <input type="checkbox" name="willdo" value="tank "/>Tank
+            </label>  
+            <label className="checkbox inline no_indent">
+              <input type="checkbox" name="willdo" value="jungle"/>Jungle
+            </label>  
+            <label className="checkbox inline no_indent">
+              <input type="checkbox" name="willdo" value="support"/>Support
+            </label>
+            <label className="checkbox inline no_indent"> 
+              <input type="checkbox" name="willdo" value="mid"/>Mid
+            </label>
+            <label className="checkbox inline no_indent">  
+              <input type="checkbox" name="willdo" value="adc"/>ADC
+            </label>
+            <label className="checkbox inline no_indent">  
+              <input type="checkbox" name="willdo" value="fill"/>Fill
+            </label>
+          </li>
+          <li>
+            <label>About Us:</label>
+            <textarea name="about" className="form-control" placeholder="Enter team description" ref="about" value={this.state.about}></textarea>
+          </li>
+          <button type="submit" className="btn btn-sml">Submit</button>
+        </form>
+      </div>
+    )
+  }
+});
+
+module.exports = TeamRegistration;
