3fd0f2141a97f2baae025d7675d74e80159f41e4
Ronnie Brown <ronnievoice@gmail.com>
2015-07-22 15:40:51 -0700

    team profile live

--- client/js/components/team/team-profile.js
+++ client/js/components/team/team-profile.js
@@ -5,59 +5,43 @@ var Axios = require('axios');
 var whiteBox = {backgroundColor: 'white', paddingTop: '10', paddingBottom: '10', margin:'0', border: 'none'};
 
 var TeamProfile = React.createClass({
-  mixins: [ Router.Navigation, Router.State ],
+  mixins: [Router.State, Router.Navigation],
+  propTypes: {
+    game: React.PropTypes.object.isRequired,
+    members: React.PropTypes.object.isRequired,
+    teamBio: React.PropTypes.object.isRequired,
+    teamName: React.PropTypes.string.isRequired
+  },
   getInitialState: function () {
-    return {
-      team: {
-        about: "",
-        times: "",
-        willdo: []
-      },
-      games: {}
-    };
+    return {};
+      
   },
   componentDidMount: function () {
     var teamToGet = '/team/' + window.location.hash.split('/')[2];
-
+    var context = this;
     Axios.get(teamToGet)
       .then(function(response) {
-          console.log(response);   
+          console.log(response.data);   
           context.setState({
+            game: response.data.game,
+            members: response.data.members,
+            // teamBio: response.data.teamBio,
+            about: response.data.teamBio.about,
             times: response.data.teamBio.times,
-            willdo: response.data.teamBio.willdo,
-            members: response.data.members
+            teamName: response.data.teamName,
           });
-          console.log(team);
       });
   },
   handleApply: function() {
 
   },
   render: function () {
-
-    var arrayToString = function(obj) {
-    var string = [];
-    for (var key in obj) {
-      if (obj[key] === true) {
-        string.push(key);
-      }
-    }
-    return string.toString();
-    };
-
-    // var available = arrayToString(this.props.times);
-    var will = arrayToString(this.props.willdo);
-    var members = arrayToString(this.props.members);
-
     return (
       <div>
         <div className="row" style={whiteBox}>
-          <div className="col-sm-offset-1 col-sm-2">
-            
-          </div>
           <div className="col-sm-4">
-            <h3>{this.props.teamName}</h3>
-            <p>{this.props.about}</p>
+            <h3>{this.state.teamName}</h3>
+            <p>{this.state.about}</p>
           </div>
           <div className="col-sm-2">
             <img className="center-block" width="128" height="128" src="/img/tier-silver.png"/>
@@ -74,9 +58,8 @@ var TeamProfile = React.createClass({
             <div className="panel panel-default" style={whiteBox}>
               <div className="panel-body">
                 <h3 className="text-center">Team Profile </h3>
-                <p><b>Plays</b>: {will}</p>
-                <p><b>Available</b>: {this.props.times} </p>
-                <p><b>About Us</b>: {this.props.about} </p>
+                <p><b>Available</b>: {this.state.times} </p>
+                <p><b>About Us</b>: {this.state.about} </p>
                 <button className="btn btn-default" type="button" onClick={this.handleApply}>Apply</button>
               </div>
             </div>
@@ -91,12 +74,13 @@ var TeamProfile = React.createClass({
           </div>
 
           <br/>
-
-          <div className="col-sm-12">
-            <div className="panel panel-default" style={whiteBox}>
-              <div className="panel-body">
-                <h3 className="text-center">Current Members</h3>
-                <p><b>individual ninjas</b>: {members} </p>
+          <div className="row">
+            <div className="col-sm-10">
+              <div className="panel panel-default" >
+                <div className="panel-body">
+                  <h3 className="text-center">Current Members</h3>
+                  <p><b>individual ninjas</b>: {this.state.members} </p>
+                </div>
               </div>
             </div>
           </div>
