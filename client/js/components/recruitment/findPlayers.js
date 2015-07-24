aea8ad72d3f60693f600244922caa2aab3b72951
bryan <bry_angelo@yahoo.com>
2015-07-24 15:33:26 -0700

    cleaned up findplayas

--- client/js/components/recruitment/findPlayers.js
+++ client/js/components/recruitment/findPlayers.js
@@ -12,7 +12,6 @@ Select = belle.Select;
 Option = belle.Option;
 Separator = belle.Separator;
 
-<<<<<<< HEAD
 var rd3 = require('react-d3');
 var LineChart = rd3.LineChart;
 
@@ -20,11 +19,9 @@ var whiteBox = {backgroundColor: 'white', padding: '25', margin:'25', border: 's
 var headshot = {backgroundColor: 'white', padding: '10', border: 'solid red 2px', height: '200', width: '200', float: 'left', textAlign: 'center'};
 var stats = {backgroundColor: 'white', padding: '25', border: 'solid blue 2px', height: '200', width: '200', display: 'block', float: 'right', textAlign: 'center'};
 var chart = {backgroundColor: 'white', padding: '25', border: 'solid blue 2px', height: '200', width: '200', display: 'block', float: 'right', textAlign: 'center'};
-=======
 var whiteBox = {backgroundColor: 'white', padding: '25', margin:'25', border: 'solid black 2px', height: '250', width: '450', display: 'inline-block'};
 var headshot = {backgroundColor: 'white', padding: '10', height: '200', width: '200', float: 'left', textAlign: 'center'};
 var stats = {backgroundColor: 'white', padding: '25', height: '200', width: '200', display: 'block', float: 'right', textAlign: 'center'};
->>>>>>> finding playas
 
 
 var FindPlayers = React.createClass({
